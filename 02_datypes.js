72f677e0c7e0ae4f559ab5208dec8a28cbaacc3c
Aman Singh <146648263+amanshh@users.noreply.github.com>
2024-01-10 00:41:49 +0000

    container config done

---
+++ 02_datypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+// alert("Hello") // we are using nodejs, not browser
+
+console.log(6
+    +5) // Code readability should be high
+console.log("Aman")
+
+
+
+let name ="Aman"
+let age=18
+let isLoggedIn =false
+
+// number ==> 2 to power 53
+// bigint 
+// boolean => true/false
+// string =>""
+// null => standalone value / representation of empty
+//  undefined =>
+// symbol => unique
+
+// object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
