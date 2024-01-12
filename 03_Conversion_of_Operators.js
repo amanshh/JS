f751110e247a3107b1db0dab6493524167453e48
Aman Singh <146648263+amanshh@users.noreply.github.com>
2024-01-12 00:23:49 +0000

    Conversion_of_Operators.js

---
+++ 03_Conversion_of_Operators.js
@@ -0,0 +1,64 @@
+let score = false // undefined //null //"45as"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// "45" ==> 45
+// "45as" => NaN
+// true ==> 1;  false ==> 0 
+
+
+let isLoggedIn ="Aman" // "" //4
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0==> false
+// "" => false
+// "Aman" ==> true
+
+
+// let someNumber = 89
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+
+// ************************** Operations *********************
+
+let value =5
+let negvalue= -value
+// console.log(negvalue);
+
+// console.log(2+5);
+// console.log(3*6);
+// console.log(7-6);
+// console.log(65/6);
+// console.log(63%2);
+
+let str1 ="Hey"
+let str2 =" Aman"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2 +2);
+// console.log(1+2 +"2");
+
+// console.log((4+5) *6 %4);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 =num2 = num3 = 2+2
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
