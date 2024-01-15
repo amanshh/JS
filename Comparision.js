f753100f55febaccad547451410bc9e58017459c
Aman Singh <146648263+amanshh@users.noreply.github.com>
2024-01-15 23:50:37 +0000

    Comparision.js

---
+++ Comparision.js
@@ -0,0 +1,21 @@
+// console.log(3<7);
+// console.log(9>=9);
+// console.log(7<9);
+// console.log(9<8);
+// console.log(66>90);
+// console.log(76>=89);
+
+
+// console.log("6" >3);
+// console.log("06" >3);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log( null >= 0);
+
+console.log(undefined ==0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//  ===  strict check 
+console.log("6"===6);
